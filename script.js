4f394c37c1692f3db84b75d3bc990431dbe70e00
Andrii <andrii.zelisko@gmail.com>
2019-10-28 14:43:11 +0200

    Update script.js
    Third task

--- script.js
+++ script.js
@@ -1,100 +1,86 @@
 'use strict';
 
-let money = Number( prompt( 'Ваш бюджет на месяц?', '100' ) );
-let date = prompt( 'Введите дату в формате YYYY-MM-DD', '2019-01-01' );
+let money, date;
+
+function start() {
+    money = prompt( 'Ваш бюджет на месяц?', '100' );
+    date = prompt( 'Введите дату в формате YYYY-MM-DD', '2019-01-01' );
+
+    while( isNaN(money) || money === '' || money == null ) {
+        money = prompt( 'Ваш бюджет на месяц?', '100' );
+    }
+}
+
+start();
 
 const appData = {
     money: money,
     timeData: date,
     expenses: {},
-    optionalExpenses: '',
+    optionalExpenses: {},
     income: '',
-    savings: false
+    savings: true
 }
 
-// for
-
-for ( let i = 0; i < 2; i++ ) {
-    let a = prompt( 'Введите обязательную статью расходов в этом месяце', 'todo smth' );
-    let b = Number( prompt( 'Во сколько обойдется?', '10' ) );
-
+function chooseExpenses() {
+    for ( let i = 0; i < 2; i++ ) {
+        let a = prompt( 'Введите обязательную статью расходов в этом месяце', 'todo smth' );
+        let b = prompt( 'Во сколько обойдется?', '10' );
+    
     if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
         && a !== '' && b !== '' && a.length < 50 && b.length < 10 ) {
-        appData.expenses[a] = b;    
-    } else {
-       i--;
+            appData.expenses[a] = b;    
+        } else {
+           i--;
+        } 
     } 
 }
 
-// do ... while
+function detectDayBudget() {
+    if (money) {
+        appData.moneyPerDay = Number(( appData.money / 30 ).toFixed());
+        alert( `бюджет на 1 день: ${appData.moneyPerDay} uah` );
+    }    
+}
 
-let i1 = 0;
-do {
-   
-    let a = prompt( 'Введите обязательную статью расходов в этом месяце', 'todo smth' );
-    let b = Number( prompt( 'Во сколько обойдется?', '10' ) );
+function checkSavings() {
+    if ( appData.savings === true ) {
+        let save = +prompt( 'Какова сума накоплений?' ),
+        percent = +prompt( 'Под какой процeнт?' );
 
-    if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
-        && a !== '' && b !== '' && a.length < 50 && b.length < 10 ) {
-        appData.expenses[a] = b;    
-    } else {
-       i--;
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert( 'Доход в месяц с вашого дупозита: ' + appData.monthIncome );
     }
-    
-    i1++;
-    
-} while ( i1 < 2 );
-
-// while
-let i2 = 0;
-while (  i2 < 2 ) {
-    let a = prompt( 'Введите обязательную статью расходов в этом месяце', 'todo smth' );
-    let b = Number( prompt( 'Во сколько обойдется?', '10' ) );
-
-    if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
-        && a !== '' && b !== '' && a.length < 50 && b.length < 10 ) {
-        appData.expenses[a] = b;    
-    } else {
-       i--;
-    } 
-    i2++;
 }
 
-appData.moneyPerDay = appData.money / 30;
+function detectLevel() {
+    if ( appData.moneyPerDay < 100 ) {
+        alert( `Уровен достатка min: ${appData.moneyPerDay}` );
+    } else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        alert( `Уровен достатка middle: ${appData.moneyPerDay}` );
+    } else if ( appData.moneyPerDay > 2000 ) {
+        alert( `Уровен достатка max: ${appData.moneyPerDay}` );
+    } else {
+        alert( `Уровен достатка Smth wrong` );
+    }
+}
 
-alert( `бюджет на 1 день: ${appData.moneyPerDay}` );
+function chooseOptExpenses() {
+    for ( let i = 0; i < 3; i++ ) {
+        let a = prompt( 'Статья необязательных расходов?', 'smth' ),
+            questionNumber = i + 1;
+        if ( typeof(a) != null && a !== '' ) {
+            appData.optionalExpenses[questionNumber] = a;
+        } else {
+            i--;
+        }
+    }
+}
+
+chooseExpenses();
+detectDayBudget();
+checkSavings();
+detectLevel();
+chooseOptExpenses();
+
 console.log( JSON.stringify( appData ) );
-
-// if, else
-
-if ( appData.moneyPerDay < 100 ) {
-    console.log( `min: ${appData.moneyPerDay}` );
-} else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ) {
-    console.log( `middle: ${appData.moneyPerDay}` );
-} else if ( appData.moneyPerDay > 2000 ) {
-    console.log( `max: ${appData.moneyPerDay}` );
-} else {
-    console.log( `Smth wrong` );
-}
-
-// switch
-
-switch( appData.moneyPerDay ) {
-    case ( appData.moneyPerDay < 100 ) : {
-        console.log( `min: ${appData.moneyPerDay}` );
-        break;
-    }
-    case ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) : {
-        console.log( `middle: ${appData.moneyPerDay}` );
-        break;
-    }
-    case ( appData.moneyPerDay > 2000 ) : {
-        console.log( `max: ${appData.moneyPerDay}` );
-        break;
-    }
-    default: {
-        console.log( `Smth wrong` );
-        break;
-    }
-}
-
